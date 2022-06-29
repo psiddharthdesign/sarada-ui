d88430de64d55a05f16dfd5cd58ad29b3a810d0c
psiddharthdesign <psiddharthdesign@gmail.com>
2022-06-29 18:43:28 +0530

    feat: create Card stories

---
+++ apps/storybook/stories/Card.stories.tsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import { Card } from 'ui';
+
+// More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export
+export default {
+  title: 'Components/Card',
+  component: Card,
+  // More on argTypes: https://storybook.js.org/docs/react/api/argtypes
+  argTypes: {
+    backgroundColor: { control: 'color' },
+  },
+};
+
+// More on component templates: https://storybook.js.org/docs/react/writing-stories/introduction#using-args
+const Template = (args) => <Card {...args} />;
+
+export const Primary = Template.bind({});
+// More on args: https://storybook.js.org/docs/react/writing-stories/args
+Primary.args = {
+  primary: true,
+  label: 'Card',
+};
+
+export const Secondary = Template.bind({});
+Secondary.args = {
+  label: 'Card',
+};
+
+export const Large = Template.bind({});
+Large.args = {
+  size: 'large',
+  label: 'Card',
+};
+
+export const Small = Template.bind({});
+Small.args = {
+  size: 'small',
+  label: 'Card',
+};
